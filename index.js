2d3b74d14ff40d6580a10b1e935158690f49a186
Mishka Hagelberg <mishka.hagelberg@gmail.com>
2026-04-02 05:53:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Bye bye, World!</h1>");
+  res.send("<h1>Hello and bye bye, World!</h1>");
 });
 
 const PORT = 8080;
